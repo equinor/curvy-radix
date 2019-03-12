487dc462dfe6a6c7894571a1bac3db651b7b82b8
Rune Kvamme <ruk@equinor.com>
2019-03-12 08:46:30 +0100

    Testing

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/curvy", methods = ['POST'] )
 def main():
 
-   print(os.environ.get('SECRET_KEY'))
+   print(os.environ.get('SECURITY_KEY'))
 
    json_input = request.get_json()
 
